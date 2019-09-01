13877f7fad16c0ea2c7d67fead2386e1d4b00363
Ted Henson <tedhenson3@gmail.com>
2019-09-01 15:09:41 -0400

    Adding AAU Stats Summarizer and preliminary model including AAU Data

---
+++ Data Cleaning/AAU Stats Summarizer.R
@@ -0,0 +1,83 @@
+
+setwd('~/Honors Thesis/Clean Data')
+
+library(readr)
+library(tidyverse)
+
+aau.data.clean = read_csv(file = 'aau.data.clean.csv')
+
+aau.data.clean = aau.data.clean %>% dplyr::filter(Player != 'Total')
+
+
+aau.data.clean = aau.data.clean %>% dplyr::select(-c(Stat.Team,
+                                                     Home.Team,
+                                                     Away.Team,
+                                                     Circuit))
+
+season.avgs = aau.data.clean %>% group_by(Player, Season) %>%
+                summarise(aau.mp = mean(MP),
+                          aau.reb = mean(REB),
+                          aau.blk  = mean(BLK),
+                          aau.stl  = mean(STL),
+                          aau.to = mean(TO),
+                          aau.pf = mean(PF),
+                          aau.pts = mean(PTS),
+                          aau.ft.made  = mean(as.numeric(word(FT, start = 1, end = 1,
+                                                  sep = '-'))),
+                          aau.fg.made  = mean(as.numeric(word(FG, start = 1, end = 1,
+                                                             sep = '-'))),
+                          aau.threep.made  = mean(as.numeric(word(`3P`, start = 1, end = 1,
+                                                             sep = '-'))))
+
+
+season.totals= aau.data.clean %>% group_by(Player, Season) %>%
+  summarise(aau.sum.mp = sum(MP),
+            aau.sum.reb = sum(REB),
+            aau.sum.blk  = sum(BLK),
+            aau.sum.stl  = sum(STL),
+            aau.sum.to = sum(TO),
+            aau.sum.pf = sum(PF),
+            aau.sum.pts = sum(PTS),
+            aau.sum.ft.made  = sum(as.numeric(word(FT, start = 1, end = 1,
+                                                sep = '-'))),
+            aau.sum.fg.made  = sum(as.numeric(word(FG, start = 1, end = 1,
+                                                sep = '-'))),
+            aau.sum.threep.made  = sum(as.numeric(word(`3P`, start = 1, end = 1,
+                                                    sep = '-'))))
+
+season.summary = inner_join(season.avgs, season.totals)
+
+season.summary = season.summary %>% arrange(Player, desc(Season))
+
+season.summary = season.summary %>% 
+  group_by(Player) %>% 
+  filter(row_number()==1)
+
+
+
+lower.player <- str_to_lower(season.summary$Player, locale = "en")
+
+lower.player <- str_replace(" jr.", "jr", lower.player)
+lower.player <- str_replace(" sr.", "sr", lower.player)
+
+lower.player <- str_replace(" iii", "iii", lower.player)
+lower.player <- str_replace(" ii", "ii", lower.player)
+lower.player <- str_replace(" iv", "iv", lower.player)
+
+
+
+lower.player <- gsub(".", "", lower.player, fixed = T)
+
+lower.player <- gsub(",iii", "iii", lower.player)
+lower.player <- gsub("'", "", lower.player, fixed  = T)
+lower.player = trimws(lower.player, which = c('both'))
+
+lower.player = gsub("\\s+", "-", lower.player)
+
+season.summary$player.id = lower.player
+
+season.summary$Season.x = season.summary$Season + 2002
+
+write.csv(season.summary, file = 'aau.season.summaries.csv', row.names = F)
+
+

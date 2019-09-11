ef24954a681922795fd5bc9d599403d6dff40205
Ted Henson <tedhenson3@gmail.com>
2019-09-11 12:04:47 -0400

    Plots of aau, prep, and win shares

---
+++ Exploratory Analysis/Prep, AAU, and bball.ref EDA.R
@@ -0,0 +1,81 @@
+
+
+setwd('~/Honors Thesis/Clean Data')
+library(readr)
+library(tidyverse)
+library(caret)
+library(caretEnsemble)
+
+
+# #note to self: do not forget to add in recreated per game prep stats
+# # as given stats are inaccurate
+# # predict offense and defense seperate
+
+
+data = read_csv('espn.bball-ref.aau.prep.csv')
+
+data = data %>% dplyr::filter(!is.na(ppg.prep) & !is.na(aau.pts)
+                              & !is.na(ws))
+
+
+data = data %>% mutate(
+  prep.ppg.actual = I(PTS / gp.max),
+  prep.mpg.actual = I(MIN / gp.max),
+  prep.blkg.actual = I(BLK / gp.max),
+  prep.stlg.actual = I(STL / gp.max),
+  prep.astg.actual = I(AST / gp.max),
+  prep.tov.actual = I(TO / gp.max),
+  prep.rebg.actual = I(REB / gp.max),
+  prep.3pmg.actual = I(`3PM` / gp.max),
+  prep.fgmg.actual = I(FGM / gp.max),
+  prep.ftmg.actual = I(FTM / gp.max),
+  prep.3pm.actual = I(`3PM` * `3P%`),
+  prep.fgm.actual = I(FGM * fg.prep),
+  prep.ftm.actual = I(FTM * `FT%`)
+)
+
+
+
+train_ind <-
+  createDataPartition(y = data$ppg.prep,
+                      p = .8,
+                      list = F)
+
+train <- data[train_ind,]
+
+
+library(ggimage)
+train$image = c("http://images.clipartpanda.com/ball-20clip-20art-basketball_clipart_1.png")
+
+#train$ws.character = as.character(train$ws)
+
+ggplot(
+  train,
+  aes(
+    x = ppg.prep,
+    y = aau.pts,
+    label = ws)) +
+  geom_image(aes(image = image), size = .02) + theme_classic() + 
+  geom_text(nudge_y = 2) + ggtitle('AAU Points versus Prep Points (WS Label)')
+
+
+ggplot(
+  train,
+  aes(
+    x = ppg.prep,
+    y = aau.pts,
+   # colour = ows,
+    label = ows)) +
+  #geom_image(aes(image = image), size = .02) + 
+  theme_classic() + 
+  #geom_point() + 
+  geom_text(nudge_y = 2, size = 3.5) + 
+  ggtitle('AAU Points versus Prep Points (OWS Label)')
+
+
+train = train %>% dplyr::select(-player.id,-Season,-TEAM,-Name)
+
+
+correlation.matrix = cor(train)
+
+test <- data[-train_ind,]
